9ec208616439b42e4309d157f4230bc910dc70ef
RDCaballero <caballerorafael211@gmail.com>
2025-04-08 21:52:47 -0700

    Delete test-env.js

--- test-env.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-console.log("Environment variables:");
-console.log({
-  SMTP_HOST: process.env.SMTP_HOST,
-  SMTP_PORT: process.env.SMTP_PORT,
-  SMTP_USER: process.env.SMTP_USER,
-  SMTP_PASS: process.env.SMTP_PASS,
-  EMAIL_FROM: process.env.EMAIL_FROM
-});
